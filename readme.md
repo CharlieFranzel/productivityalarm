78e975d5428ae96e81f749175189e87c01a30a95
charliefranzel <charliefranzel@gmail.com>
2024-10-03 22:50:22 -0600

    more stuff

---
+++ readme.md
@@ -0,0 +1,7 @@
+Hi!
+
+This is the code for my little alarm clock project that -- lets be honest, wont ever get finished -- however yay code.
+
+Instructions:
+The primary.py file is the code that makes it run. (dont mess with that)
+Running the settings_wizard.py file allows you to configure the settings
---
+++ settings_wizard.py
@@ -0,0 +1,18 @@
+
+create = open('settings.txt','a')  # Creates the sttings file
+settings = open("settings.txt", "a")
+
+with open('settings.txt', 'w') as file: # This clears all old contents
+    pass
+
+text1 = str(input('How many questions need to be answered correctly to stop alarm?: '))   # Prompts and writes inputs
+settings.write(text1)
+settings.write('\n')
+text2 = str(input('How many correct answers should the penalty for getting one wrong be?: '))
+settings.write(text2)
+settings.write('\n')
+text3 = str(input('How many minutes until the alrm should shut itself off?: '))
+settings.write(text3)
+print('Completed Successfully!')
+
+settings.close()
